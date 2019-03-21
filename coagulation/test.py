9a6a0746523b256158365a54f730a76c8defe634
paolaallegrini <paola.allegrini@laposte.net>
2019-03-21 18:04:10 +0100

    start coagulation simple

---
+++ coagulation/test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 16:34:02 2019
+
+@author: Home
+"""
+
+import numpy as np 
+Ns=10
+a=np.ones(Ns)
+b=np.ones(Ns)
+U=[20,2]
+print(a*b)
+print('---test----')
+print(np.size(U))
+n=20
+N=30
+print(n,N)
